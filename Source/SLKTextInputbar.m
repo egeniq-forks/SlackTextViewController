56d802d7536f33a8c60cc5fcbcfd7e61e86ddd54
Harold Asiimwe <harryasiimwe@gmail.com>
2019-09-02 21:30:41 +0300

    alignment fix

--- Source/SLKTextInputbar.m
+++ Source/SLKTextInputbar.m
@@ -738,13 +738,13 @@ NSString * const SLKTextInputbarDidMoveNotification =   @"SLKTextInputbarDidMove
         CGSize audioButtonSize = [self.audioButton imageForState:self.audioButton.state].size;
         if (audioButtonSize.width > 0) {
             self.audioButtonHC.constant = roundf(audioButtonSize.height) + extraButtonHeight;
-            self.audioButtonBottomMarginC.constant = roundf((self.intrinsicContentSize.height - audioButtonSize.height) / 2.0) + self.slk_contentViewHeight / 3.0;
+            self.audioButtonBottomMarginC.constant = roundf((self.intrinsicContentSize.height - audioButtonSize.height) / 2.0) + self.slk_contentViewHeight / 2.0;
         }
         
         CGSize cameraButtonSize = [self.cameraButton imageForState:self.cameraButton.state].size;
         if (cameraButtonSize.width > 0) {
             self.cameraButtonHC.constant = roundf(cameraButtonSize.height) + extraButtonHeight;
-            self.cameraButtonBottomMarginC.constant = roundf((self.intrinsicContentSize.height - cameraButtonSize.height) / 2.0) + self.slk_contentViewHeight / 3.0;
+            self.cameraButtonBottomMarginC.constant = roundf((self.intrinsicContentSize.height - cameraButtonSize.height) / 2.0) + self.slk_contentViewHeight / 2.0;
         }
         
         self.audioButtonWC.constant = roundf(audioButtonSize.width) + extraButtonWidth;
